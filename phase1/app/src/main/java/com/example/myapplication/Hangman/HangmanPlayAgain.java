3074a130e983b9d9a881132152c32c8be856e329
Patrick Louis <patrick.louis@mail.utoronto.ca>
2019-10-31 18:43:38 -0400

    Implement method onCreate

--- phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanPlayAgain.java
+++ phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanPlayAgain.java
@@ -2,7 +2,11 @@ package com.example.myapplication.Hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HangmanPlayAgain extends AppCompatActivity {
 
@@ -14,5 +18,50 @@ public class HangmanPlayAgain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman_play_again);
+
+        Intent intent = getIntent();
+        String score = intent.getStringExtra(HangmanGame.getScoreMessage());
+        String message = intent.getStringExtra(HangmanGame.getMessage());
+        hangmanGameStat = intent.getParcelableExtra(HangmanMain.getGamestatusMsg());
+        assert hangmanGameStat != null;
+        originalGender = hangmanGameStat.getGender();
+
+        TextView txtScore = findViewById(R.id.txtScore);
+        txtScore.setText(score);
+
+        TextView txtMessage = findViewById(R.id.txtMessage);
+        txtMessage.setText(message);
+
+        Button playAgain = findViewById(R.id.btnPlayAgain);
+        Button mainMenu = findViewById(R.id.btnMainMenu);
+
+        hangmanGameStat.resetGameStatus();
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HangmanGame.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                hangmanGameStat.resetGameStatus();
+                hangmanGameStat.setGender(originalGender);
+                intent.putExtra(HangmanMain.getGamestatusMsg(), hangmanGameStat);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        mainMenu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), HangmanMain.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                hangmanGameStat.resetGameStatus();
+                hangmanGameStat.setGender(originalGender);
+                intent.putExtra(HangmanMain.getGamestatusMsg(), hangmanGameStat);
+                intent.putExtra("clear game", true);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
