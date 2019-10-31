282330ba87a07df3131a5abe7e1a6fa6dca6b9e6
Patrick Louis <patrick.louis@mail.utoronto.ca>
2019-10-31 18:57:53 -0400

    Implement onCreate and onClick

--- phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanSetting.java
+++ phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanSetting.java
@@ -2,9 +2,14 @@ package com.example.myapplication.Hangman;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class HangmanSetting extends AppCompatActivity {
+import com.example.myapplication.R;
+
+public class HangmanSetting extends AppCompatActivity implements View.OnClickListener {
 
     private HangmanGameStat hangmanGameStat;
 
@@ -12,5 +17,29 @@ public class HangmanSetting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hangman_setting);
+
+        Intent intent = getIntent();
+        hangmanGameStat = intent.getParcelableExtra(HangmanMain.getGamestatusMsg());
+
+        // get which gender the player chose to play with
+        Button btnMale = findViewById(R.id.btnMale);
+        Button btnFemale = findViewById(R.id.btnFemale);
+
+        btnMale.setOnClickListener(this);
+        btnFemale.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(getApplicationContext(), HangmanGame.class);
+
+        if (view.getId() == R.id.btnFemale) {
+            hangmanGameStat.setGender("FEMALE");
+            intent.putExtra(HangmanMain.getGamestatusMsg(), hangmanGameStat);
+        } else {
+            hangmanGameStat.setGender("MALE");
+            intent.putExtra(HangmanMain.getGamestatusMsg(), hangmanGameStat);
+        }
+        startActivity(intent);
     }
 }
