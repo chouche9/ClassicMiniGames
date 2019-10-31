1b27487bcaecbee66055dd9e53bcb932bff6199d
Patrick Louis <patrick.louis@mail.utoronto.ca>
2019-10-31 18:58:51 -0400

    Instantiate variable

---
+++ phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanWordGenerator.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.Hangman;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+
+public class HangmanWordGenerator {
+
+    private static final String WORDS_FILE = "words_database.txt";
+    private static final String TAG = "Main Activity";
+
+    private ArrayList<String> listOfWords = new ArrayList<String>();
+    private Context context;
+}
