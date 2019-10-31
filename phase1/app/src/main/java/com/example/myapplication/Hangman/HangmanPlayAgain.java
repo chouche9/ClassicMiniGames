602641e1ee98f3e1d819155961ec55daba0f00f8
Patrick Louis <patrick.louis@mail.utoronto.ca>
2019-10-31 18:41:00 -0400

    Do the UI for playagain

---
+++ phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanPlayAgain.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.Hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HangmanPlayAgain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangman_play_again);
+    }
+}
