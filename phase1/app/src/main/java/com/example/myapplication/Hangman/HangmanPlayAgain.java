347b9615b24cf888c2665725e1e7f5a2f0de6cfb
Patrick Louis <patrick.louis@mail.utoronto.ca>
2019-10-31 18:49:32 -0400

    Updating hangman Settings

--- phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanPlayAgain.java
+++ phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanPlayAgain.java
@@ -8,6 +8,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.example.myapplication.R;
+
 public class HangmanPlayAgain extends AppCompatActivity {
 
     // Instatiate Variables
---
+++ phase1/app/src/main/java/com/example/myapplication/Hangman/HangmanSetting.java
@@ -0,0 +1,14 @@
+package com.example.myapplication.Hangman;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HangmanSetting extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hangman_setting);
+    }
+}
