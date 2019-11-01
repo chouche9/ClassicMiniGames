1c940ae67e6838756e4c9e6efdb9c2e7a638252d
Seoyoon Jung <ariana.jung@mail.utoronto.ca>
2019-11-01 16:50:46 -0400

    Added javadoc for UserManager.java.

--- phase1/app/src/main/java/com/example/myapplication/UserManager.java
+++ phase1/app/src/main/java/com/example/myapplication/UserManager.java
@@ -10,21 +10,43 @@ import com.google.gson.Gson;
 
 import java.util.Map;
 
+/**
+ * A UserManager for this application.
+ */
 public class UserManager {
+    /**
+     * SharedPreferences that stores all users.
+     */
     private SharedPreferences sharedPreferences;
+
+    /**
+     * Editor that edits the sharedPreferences.
+     */
     private SharedPreferences.Editor editor;
+
+    /**
+     * The single static instance of UserManager.
+     */
     private static UserManager userManager = null;
+
+    /**
+     * Construct this UserManager singleton instance.
+     *
+     * @param activity the activity that called this UserManager.
+     */
     private UserManager(Activity activity) {
-        this.sharedPreferences = activity.getApplicationContext().getSharedPreferences("users", Context.MODE_PRIVATE);
+        this.sharedPreferences = activity.getApplicationContext().
+                getSharedPreferences("users", Context.MODE_PRIVATE);
         editor = sharedPreferences.edit();
+        editor.apply();
     }
-    // static variable single_instance of type Singleton
 
-    // variable of type String
-    public String s;
-
-    // private constructor restricted to this class itself
-    // static method to create instance of Singleton class
+    /**
+     * Method that creates the only instance of UserManager.
+     *
+     * @param activity the activity that called this UserManager.
+     * @return UserManager the only instance of UserManager.
+     */
     public static UserManager getInstance(Activity activity)
     {
         if (userManager == null)
@@ -33,30 +55,62 @@ public class UserManager {
         return userManager;
     }
 
+    /**
+     * Saves the User instance into sharedPreferences.
+     *
+     * @param user the user that is to be saved.
+     */
     public void saveUsers(User user){
         Gson gson = new Gson();
         String json = gson.toJson(user);
         editor.putString(user.getName(), json);
         editor.apply();
     }
-    public User getUser(String user_name){
+
+    /**
+     * Return the User instance with username userName.
+     *
+     * @param userName the username of a user.
+     * @return User the user instance with the username userName.
+     */
+    public User getUser(String userName){
         Gson gson = new Gson();
-        String json = sharedPreferences.getString(user_name, null);
+        String json = sharedPreferences.getString(userName, null);
         User user = gson.fromJson(json, User.class);
         return user;
     }
-    public boolean authenticate(String user_name, String password){
-        User user = getUser(user_name);
+
+    /**
+     * Return whether if userName and password are valid.
+     *
+     * @param userName the username.
+     * @param password the password.
+     * @return true if userName and password are valid.
+     */
+    public boolean authenticate(String userName, String password){
+        User user = getUser(userName);
         if (user == null){
             return false;
         }
         return (user.getPassword().equals(password));
     }
+
+    /**
+     * Return whether if a user with a username userName has an account already.
+     *
+     * @param userName the username.
+     * @return true if a user with userName doesn't exist.
+     */
     public boolean createAuthenticate(String userName){
         User user = userManager.getUser(userName);
         return user == null;
     }
 
+    /**
+     * Return a string representation of all users in this application.
+     *
+     * @return a string representation of all users in this application.
+     */
     @NonNull
     @Override
     public String toString() {
