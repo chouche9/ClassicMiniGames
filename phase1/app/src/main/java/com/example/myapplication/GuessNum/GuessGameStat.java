dc16676f7e4b763e73a4a0a27b5df9284a91a520
Yingke Wang <yingkewang.wang@mail.utoronto.ca>
2019-11-01 18:23:33 -0400

    formalize docstring in GuessNum package

--- phase1/app/src/main/java/com/example/myapplication/GuessNum/GuessGameStat.java
+++ phase1/app/src/main/java/com/example/myapplication/GuessNum/GuessGameStat.java
@@ -6,19 +6,46 @@ import android.os.Parcelable;
 import com.example.myapplication.GameStatus;
 
 public class GuessGameStat extends GameStatus implements Parcelable {
+    /**
+     * color of the Text for the GuessGame player choose
+     */
     private String color;
+    /**
+     * the current Tries of this player
+     */
     private int currentTries = 0;
+    /**
+     * the best tries of this player
+     */
     private int bestTries = -1;
+    /**
+     * binary indicate whether player finish the last game or not. 0 represent finish, 1 represent
+     * not finish
+     */
     private int played = 0;
+    /**
+     * the target number for this player on the last game if he did not finish
+     */
     private int lastNum;
+    /**
+     * type of the class in string
+     */
     private String type;
 
-    public GuessGameStat(String name){
+    /**
+     * construct GuessGameStat instance for user: name
+     * @param name name of the user
+     */
+    GuessGameStat(String name){
         super(name);
         this.type = "GuessGameStat";
     }
 
-    protected GuessGameStat(Parcel in) {
+    /**
+     * build GuessGameStat from Parcel
+     * @param in the Parcel that store GuessGameStat
+     */
+    private GuessGameStat(Parcel in) {
         super(in);
         color = in.readString();
         currentTries = in.readInt();
@@ -28,14 +55,25 @@ public class GuessGameStat extends GameStatus implements Parcelable {
         type = in.readString();
     }
 
+    /**
+     * the the number of target Number when exiting the game
+     * @param lastNum the target Number that in the game that just pause
+     */
     public void setLastNum(int lastNum) {
         this.lastNum = lastNum;
     }
 
+    /**
+     * get the target Number in the paused game
+     * @return
+     */
     public int getLastNum() {
         return lastNum;
     }
 
+    /**
+     * create GuessGameStat by the super Creator object
+     */
     public static final Creator<GuessGameStat> CREATOR = new Creator<GuessGameStat>() {
         @Override
         public GuessGameStat createFromParcel(Parcel in) {
@@ -48,26 +86,52 @@ public class GuessGameStat extends GameStatus implements Parcelable {
         }
     };
 
-    public void setCurrentTries() {
+    /**
+     * update currentTries by increasing 1
+     */
+    void setCurrentTries() {
         this.currentTries ++;
     }
+
+    /**
+     * set the current tries to cur
+     * @param cur the new current tries
+     */
     public void setCurrentTries(int cur) {
         this.currentTries  = cur;
     }
+
+    /**
+     * set attribute color to colr
+     * @param color the color want to set
+     */
 
     public void setColor(String color) {
         this.color = color;
     }
 
+    /**
+     * get the color player choose
+     * @return the color in String
+     */
 
     public String getColor() {
         return color;
     }
 
-    public int getBestTries() {
+    /**
+     * get the best tries for this player
+     * @return the best tries number
+     */
+     int getBestTries() {
         return bestTries;
     }
-    public boolean played(){
+
+    /**
+     *
+     * @return whether this player has a pause game or not
+     */
+     boolean played(){
         if (played == 0){
             return false;
         }
@@ -76,12 +140,20 @@ public class GuessGameStat extends GameStatus implements Parcelable {
         }
     }
 
-
+    /**
+     * default method for implementing parcelable
+     * @return the special content in parcelable
+     */
     @Override
     public int describeContents() {
         return 0;
     }
 
+    /**
+     * write attribute of GuessGameStat into parcel
+     * @param parcel parcel to write the attributes of this GameStatus.
+     * @param i flags.
+     */
     @Override
     public void writeToParcel(Parcel parcel, int i) {
         super.writeToParcel(parcel, i);
@@ -92,7 +164,12 @@ public class GuessGameStat extends GameStatus implements Parcelable {
         parcel.writeInt(lastNum);
         parcel.writeString(type);
     }
-    public void finishUpdate(int finalTries){
+
+    /**
+     * update GuessGameStat when finish guessing the number
+     * @param finalTries: the total tries to get the right answer
+     */
+     void finishUpdate(int finalTries){
         if (bestTries == -1 || bestTries > finalTries){
             bestTries = finalTries;
         }
@@ -100,14 +177,28 @@ public class GuessGameStat extends GameStatus implements Parcelable {
         lastNum = 0;
         played = 0;
     }
-    public void startUpdate(){
+
+    /**
+     * update GuessGameStat when player start a new round
+     */
+     void startUpdate(){
         this.setPlayed(true);
         this.setCurrentTries(0);
         this.setLastNum(0);
     }
-    public int getCurrentTries(){
+
+    /**
+     *
+     * @return the current tries;
+     */
+     int getCurrentTries(){
         return currentTries;
     }
+
+    /**
+     * set whether it has a pause game or not
+     * @param played the indicator of having a paused game
+     */
 
     public void setPlayed(boolean played) {
         if(played){
